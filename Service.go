55c7dff67ee59ff5c53347fd99645ed1cc9dba1e
John Driscoll <git@jdd.email>
2020-11-03 14:51:47 +0000

    Early prototyping

--- Service.go
+++ Service.go
@@ -4,10 +4,28 @@ import "net/http"
 
 	type Service struct {
 
+		routes []route
+	}
 
+	func (s *Service) AddRoute(path string, handler Handler) {
+
+		if len(s.routes) < 1 {
+
+			s.routes = make([]route, 0)
+		}
+
+		s.routes = append(s.routes, route{ Path: path, Handler: handler })
 	}
 
 	func (s *Service) Handler(w http.ResponseWriter, r *http.Request) {
 
-		w.Write([]byte("works"))
+		response := s.routes[0].Handler(Request{ input: r, output: w })
+		w.WriteHeader(response.StatusCode)
+		w.Write(response.Content)
+	}
+
+	type route struct {
+
+		Path string
+		Handler Handler
 	}
